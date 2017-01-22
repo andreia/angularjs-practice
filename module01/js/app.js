4585639cb06a3c470a750124106023c161029a19
Andreia Bohner <andreiabohner@gmail.com>
2017-01-22 17:23:58 -0200

    Add module 01

---
+++ module01/js/app.js
@@ -0,0 +1,65 @@
+(function () {
+'use strict';
+
+angular.module('LunchCheckerApp', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope) {
+    $scope.items        = "";
+    $scope.message      = "";
+    $scope.textboxClass = "";
+    $scope.messageClass = "";
+
+    $scope.checkItems = function () {
+        $scope.message      = sayMessage($scope.items);
+        $scope.textboxClass = getTextboxClass($scope.items);
+        $scope.messageClass = getMessageClass($scope.items);
+    };
+
+    $scope.reset = function () {
+        $scope.message      = "";
+        $scope.textboxClass = "";
+        $scope.messageClass = "";
+    }
+
+    function sayMessage(items) {
+        if (items == "") {
+            return "Please enter data first";
+        }
+
+        var arrayOfItems    = itemsToArray($scope.items);
+
+        return getMessage(arrayOfItems.length);
+    }
+
+    function getMessage(itemsNumber) {
+        if (itemsNumber > 3) {
+            return "Too much!";
+        }
+
+        return "Enjoy!";
+    }
+
+    function getMessageClass(items) {
+        if (items) {
+            return "text-success";
+        }
+
+        return "text-danger";
+    }
+
+    function getTextboxClass(items) {
+        if (items) {
+            return "has-success";
+        }
+
+        return "has-error";
+    }
+
+    function itemsToArray(items) {
+        return items.split(',');
+    }
+}
+
+})();
